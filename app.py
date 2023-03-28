d42cdd9f59ac553c5f44a745fa58d574b0dc7bdc
sojk401 <sojk401@gmail.com>
2023-03-28 18:46:38 +0900

    [Feat] DB에서 로그인한 회원 조회 기능 추가

--- app.py
+++ app.py
@@ -25,13 +25,13 @@ def home() :
     #     token_receive = request.cookies.get('mytoken')
     # try:
     #     payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-    #     user_info = db.users.find_one({"id": payload['id']})
+    #     user_info = db.user.find_one({"id": payload['id']})
     #     return render_template('index.html', nickname=user_info["nickname"])
     # except jwt.ExpiredSignatureError:
     #     return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
     # except jwt.exceptions.DecodeError:
     #     return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
-	return render_template('review.html')
+	return render_template('index.html')
 
 #################################
 ##  리뷰 관련 API               ##
@@ -85,8 +85,40 @@ def api_register():
         'nickname': nickname_receive
     }
 
-    db.users.insert_one(info)
+    db.user.insert_one(info)
     return jsonify({'result': 'success'})
+
+# [로그인 API]
+# id, pw를 받아서 맞춰보고, 토큰을 만들어 발급
+@app.route('/api/login', methods=['POST'])
+def api_login():
+    email_receive = request.form['email_give']
+    pw_receive = request.form['pw_give']
+
+    # pw 암호화
+    pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
+
+    # id, 암호화된pw을 가지고 해당 유저 찾기
+    result = db.user.find_one({'email': email_receive, 'password': pw_hash})
+
+    # 찾으면 JWT 토큰을 만들어 발급
+    if result is not None:
+        # JWT 토큰에는, payload와 시크릿키가 필요합니다.
+        # 시크릿키가 있어야 토큰을 디코딩(=풀기) 해서 payload 값을 볼 수 있습니다.
+        # 아래에선 id와 exp를 담았습니다. 즉, JWT 토큰을 풀면 유저ID 값을 알 수 있습니다.
+        # exp에는 만료시간을 넣어줍니다. 만료시간이 지나면, 시크릿키로 토큰을 풀 때 만료되었다고 에러가 납니다.
+        # 만료시간 1시간으로 설정
+        payload = {
+            'email': email_receive,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=1)
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+
+        # token을 줍니다.
+        return jsonify({'result': 'success', 'token': token})
+    # 찾지 못하면
+    else:
+        return jsonify({'result': 'fail', 'msg': '로그인 또는 비밀번호가 일치하지 않습니다.'})
 
 if __name__ == '__main__' :
 		app.run('0.0.0.0', port = 5000, debug = True)
